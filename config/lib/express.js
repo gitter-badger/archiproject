f653f8e19d6bfe31e764cc44a44885ee6316ca28
Tristan Fitzgerald <tristan@simplyspoke.com>
2017-01-18 15:27:57 -0600

    A few tweaks

--- config/lib/express.js
+++ config/lib/express.js
@@ -33,13 +33,9 @@ module.exports.initLocalVariables = function (app) {
   }
   app.locals.keywords = config.app.keywords;
   app.locals.googleAnalyticsTrackingID = config.app.googleAnalyticsTrackingID;
-  app.locals.facebookAppId = config.facebook.clientID;
-  app.locals.twitterUsername = config.twitter.username;
   app.locals.jsFiles = config.files.client.js;
   app.locals.cssFiles = config.files.client.css;
   app.locals.livereload = config.livereload;
-  app.locals.logo = config.logo;
-  app.locals.favicon = config.favicon;
   app.locals.env = process.env.NODE_ENV;
   app.locals.domain = config.domain;
 
@@ -64,7 +60,7 @@ module.exports.initMiddleware = function (app) {
   }));
 
   // Initialize favicon middleware
-  app.use(favicon(app.locals.favicon));
+  // app.use(favicon(app.locals.favicon));
 
   // Enable logger (morgan) if enabled in the configuration file
   if (_.has(config, 'log.format')) {
