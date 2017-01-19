383788a92dec39d7802f8b634def088324a8d0b1
Tristan Fitzgerald <tristan@simplyspoke.com>
2017-01-19 15:05:25 -0600

    Began configuring the harvest connection.

--- modules/projects/server/config/projects.server.config.js
+++ modules/projects/server/config/projects.server.config.js
@@ -4,11 +4,29 @@
  * Module dependencies
  */
 var path = require('path'),
-  config = require(path.resolve('./config/config'));
+  config = require(path.resolve('./config/config')),
+  Harvest = require('harvest');
 
 /**
  * Module init function.
  */
-module.exports = function (app, db) {
+module.exports = function(app, db) {
+  app.harvest = new Harvest({
+    subdomain: config.harvest_oauth.subdomain,
+    identifier: config.harvest_oauth.client_id,
+    secret: config.harvest_oauth.secret,
+    redirect_uri: config.harvest_oauth.redirect_uri
+  });
 
+  // app.harvest.parseAccessCode({}, function(access_token) {
+  //   console.log('Grabbed the access token', access_token);
+
+  var TimeTracking = app.harvest.TimeTracking;
+
+  TimeTracking.daily({}, function(err, tasks) {
+    if (err) throw new Error(err);
+    console.log(tasks);
+    console.log('Loaded tasks using oauth!');
+  });
+  // });
 };
--- modules/projects/server/routes/projects.server.routes.js
+++ modules/projects/server/routes/projects.server.routes.js
@@ -6,11 +6,14 @@
 var projectsPolicy = require('../policies/projects.server.policy'),
   projects = require('../controllers/projects.server.controller');
 
-module.exports = function (app) {
+module.exports = function(app) {
   // Projects collection routes
   app.route('/api/projects').all(projectsPolicy.isAllowed)
     .get(projects.list)
     .post(projects.create);
+
+  app.route('/api/projects/harvest/auth').all(projectsPolicy.isAllowed).get(projects.oauthCall);
+  app.route('/api/projects/harvest/auth/callback').all(projectsPolicy.isAllowed).get(projects.oauthCallback);
 
   // Single project routes
   app.route('/api/projects/:projectId').all(projectsPolicy.isAllowed)
